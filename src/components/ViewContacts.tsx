3dadf181ffe9d9bfad285d0bac7fb768a5fe8f92
websitesByPatrick <98294337+websitesByPatrick@users.noreply.github.com>
2025-01-01 12:49:11 -0600

    update 1/1/2025 v3

--- src/components/ViewContacts.tsx
+++ src/components/ViewContacts.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from "react";
-import { Button } from "@/components/Button";
-import Link from "next/link";
+
+
 import { redirect } from "next/navigation";
 interface Props {
   data: Array<{
